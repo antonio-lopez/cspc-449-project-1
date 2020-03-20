5c76bde6f25e4b50ded80d8d956edfe3753050e1
zhuangzexin <zexinzhuang@gmail.com>
2020-03-20 16:11:10 -0700

    posting microservice:
    •Create a new post (DONE)
    •Delete an existing post (DONE)
    •Retrieve an existing post (DONE)
    •List the n most recent posts to a particular community (DONE)
    •List the n most recent posts to any community (TO BE DISCUSSED)
    •Curl commands (DONE)

--- reddit.py
+++ reddit.py
@@ -18,66 +18,6 @@ def home():
 <p>WeLcOmE tO rEdDiT</p>'''
 
 
-@app.route('/api/v1/resources/users/all', methods=['GET'])
-def api_all():
-    conn = sqlite3.connect('useraccount.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    all_users = cur.execute('SELECT * FROM useraccount;').fetchall()
-
-    return jsonify(all_users)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
-
-
-@app.route('/api/v1/resources/users/all', methods=['POST'])
-def create_user():
-    request_json = request.get_json()
-    Email = request_json.get('Email')
-    User = request_json.get('User')
-    Karma = request_json.get('Karma')
-
-    # query_parameters = request.args
-    # Email = query_parameters.get('Email')
-    # User = query_parameters.get('User')
-    # Karma = query_parameters.get('Karma')
-    # query = ''' INSERT INTO useraccount VALUES(?,?,?) '''
-    # to_filter = []
-    # if Email:
-    #     # query += ' Email=? AND'
-    #     to_filter.append(Email)
-    # if User:
-    #     # query += ' published=? AND'
-    #     to_filter.append(User)
-    # if Karma:
-    #     # query += ' author=? AND'
-    #     to_filter.append(Karma)
-
-    conn = sqlite3.connect('useraccount.db')
-    cur = conn.cursor()
-
-    response_content = cur.execute("INSERT INTO useraccount VALUES(?,?,?)", (Karma, User, Email))
-    conn.commit()
-    # return jsonify(response_content)
-    return Response(response_content, status=201, mimetype='application/json')
-
-
-@app.route('/api/v1/resources/users/email', methods=['PUT'])
-def update_email():
-    request_json = request.get_json()
-    User = request_json.get('User')
-    Email = request_json.get('Email')
-
-    conn = sqlite3.connect('useraccount.db')
-    cur = conn.cursor()
-    response_content = cur.execute("UPDATE useraccount SET Email=? WHERE User=?", (Email, User))
-    conn.commit()
-    return Response(response_content, status=200, mimetype='application/json')
-
-
 @app.route('/api/v1/resources/post/all_post', methods=['GET'])
 def all_post():
     conn = sqlite3.connect('post.db')
@@ -138,6 +78,37 @@ def retrieve_post():
         postId = data['postId']
         post = cur.execute('''SELECT * FROM userpost 
                         WHERE postId=?;''', (postId,)).fetchall()
+        conn.commit()
+        cur.close()
+        conn.close()
+        return jsonify(post)
+    except Exception as e:
+        return jsonify(str(e))
+
+@app.route('/api/v1/resources/post/listNthIntoCommunity', methods=['POST'])
+def listNthIntoCommunity():
+    data = request.get_json()
+    database = "post.db"
+    try:
+        conn = sqlite3.connect(database)
+        cur = conn.cursor()
+        nth = data['nth']
+        thisCom = data['community']
+        post = cur.execute('''WITH myTableWithRows AS (
+            SELECT (ROW_NUMBER() OVER (ORDER BY userpost.date DESC)) as row,*
+            FROM userpost)
+            SELECT * FROM myTableWithRows WHERE row <= ?''', (nth,)).fetchall()
+
+        for i in range(nth):
+            unix = time.time()
+            date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+            thisTitle = post[i][2]
+            thisText = post[i][3]
+            thisUrl = post[i][5]
+            thisUsername = post[i][6]
+            cur.execute('''INSERT INTO userpost (title,text,community,url,username,date)
+                                VALUES(?,?,?,?,?,?)''', (thisTitle, thisText, thisCom, thisUrl, thisUsername, date))
+
         conn.commit()
         cur.close()
         conn.close()
