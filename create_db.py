268a7923bdc7ed87e0e974cee91560318865949d
Javier Melendrez <44476129+javimelendrez@users.noreply.github.com>
2020-04-21 20:21:55 -0700

    updated database script

--- create_db.py
+++ create_db.py
@@ -17,6 +17,14 @@ conn.execute('''CREATE TABLE user_tbl
          email  TEXT NOT NULL UNIQUE,
          karma  INTEGER NOT NULL);''')
 
+conn.execute('''CREATE TABLE message_tbl
+         (messageid INTEGER PRIMARY KEY UNIQUE,
+         userto  TEXT   NOT NULL UNIQUE,
+         userfrom   TEXT   NOT NULL UNIQUE,
+         messagecontents  TEXT   NOT NULL,
+         messageflag    TEXT NOT NULL,
+         date   TEXT    NOT NULL);''')
+
 print("Database created successfully")
 
 conn.commit()
